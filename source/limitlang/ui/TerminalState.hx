5ada6ba19155805834994f50fc89f7404564da7e
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-18 02:47:02 -0500

    typing

--- source/limitlang/ui/TerminalState.hx
+++ source/limitlang/ui/TerminalState.hx
@@ -1,5 +1,6 @@
 package limitlang.ui;
 
+import limitlang.util.Constants;
 import flixel.input.keyboard.FlxKey;
 import flixel.FlxG;
 import flixel.text.FlxText;
@@ -13,19 +14,58 @@ class TerminalState extends FlxState
 	{
 		super.create();
 
-		inputText = new FlxText(0, 0, 0, 'inputText', 16);
+		inputText = new FlxText(0, 0, FlxG.width, 'inputText', 16);
 		add(inputText);
-		inputText.y = FlxG.height - inputText.height;
+
+		backspaceTmr = backspaceTmrStart;
 	}
+
+	final backspaceTmrStart:Int = 300;
+	final backspaceTmrDecrease:Int = 10;
+	var backspaceTmr:Int = 0;
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		inputText.y = FlxG.height - inputText.height;
 
-        if (FlxG.keys.justPressed.ANY)
-        {
-            var key:FlxKey = FlxG.keys.firstJustPressed();
-            trace(key);
-        }
+		if (FlxG.keys.justPressed.ANY)
+		{
+			var key:FlxKey = FlxG.keys.firstJustPressed();
+
+			onKey(key);
+		}
+
+		if (FlxG.keys.pressed.BACKSPACE)
+		{
+			if (backspaceTmr <= 0)
+				onKey(BACKSPACE);
+			else
+				backspaceTmr -= backspaceTmrDecrease;
+		}
+		if (FlxG.keys.justReleased.BACKSPACE)
+			backspaceTmr = backspaceTmrStart;
+	}
+
+	function onKey(key:FlxKey)
+	{
+		switch (key)
+		{
+			case END, BREAK, MENU, SHIFT, ENTER, ALT, CONTROL, WINDOWS, SCROLL_LOCK, NUMLOCK, TAB, CAPSLOCK, LEFT, DOWN, UP, RIGHT, DELETE, PRINTSCREEN,
+				PAGEUP, PAGEDOWN, INSERT:
+
+			case BACKSPACE:
+				inputText.text = inputText.text.substr(0, inputText.text.length - 1);
+
+			default:
+				if (Constants.KEY_SUPPLEMENTS.exists(key))
+					if ((FlxG.keys.pressed.SHIFT && Constants.KEY_SHIFT_SUPPLEMENTS.exists(key)))
+						inputText.text += Constants.KEY_SHIFT_SUPPLEMENTS.get(key)
+					else
+						inputText.text += Constants.KEY_SUPPLEMENTS.get(key);
+				else
+					inputText.text += (FlxG.keys.pressed.SHIFT || Constants.FUNCTION_KEYS.contains(key)) ? key.toString()
+						.toUpperCase() : key.toString().toLowerCase();
+		}
 	}
 }
--- source/limitlang/util/Constants.hx
+++ source/limitlang/util/Constants.hx
@@ -1,6 +1,68 @@
 package limitlang.util;
 
+import flixel.input.keyboard.FlxKey;
+
 class Constants
 {
 	public static final CONTENT_FOLDER = 'content';
+
+	public static final KEY_SUPPLEMENTS:Map<FlxKey, String> = [
+		GRAVEACCENT => '`',
+		ONE => '1',
+		TWO => '2',
+		THREE => '3',
+		FOUR => '4',
+		FIVE => '5',
+		SIX => '6',
+		SEVEN => '7',
+		EIGHT => '8',
+		NINE => '9',
+		ZERO => '0',
+		NUMPADONE => '1',
+		NUMPADTWO => '2',
+		NUMPADTHREE => '3',
+		NUMPADFOUR => '4',
+		NUMPADFIVE => '5',
+		NUMPADSIX => '6',
+		NUMPADSEVEN => '7',
+		NUMPADEIGHT => '8',
+		NUMPADNINE => '9',
+		NUMPADZERO => '0',
+		MINUS => '-',
+		PLUS => '=',
+		LBRACKET => '[',
+		RBRACKET => ']',
+		BACKSLASH => '\\',
+		SEMICOLON => ';',
+		QUOTE => '\'',
+		COMMA => ',',
+		PERIOD => '.',
+		SLASH => '/',
+		SPACE => ' ',
+	];
+	public static final KEY_SHIFT_SUPPLEMENTS:Map<FlxKey, String> = [
+		GRAVEACCENT => '~',
+		ONE => '!',
+		TWO => '@',
+		THREE => '#',
+		FOUR => '$',
+		FIVE => '%',
+		SIX => '^',
+		SEVEN => '&',
+		EIGHT => '*',
+		NINE => '(',
+		ZERO => ')',
+		MINUS => '_',
+		PLUS => '+',
+		LBRACKET => '{',
+		RBRACKET => '}',
+		BACKSLASH => '|',
+		SEMICOLON => ':',
+		QUOTE => '"',
+		COMMA => '<',
+		PERIOD => '>',
+		SLASH => '?',
+	];
+
+	public static final FUNCTION_KEYS:Array<FlxKey> = [F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12];
 }
